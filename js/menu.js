df2097531c31e1791f5d504da7b249f2fc7e1309
EvansJLR <evansjavierlr@gmail.com>
2024-04-22 18:27:27 +0200

    Prueba 2. Descartando errores. ¿Filtro?

--- js/menu.js
+++ js/menu.js
@@ -14,30 +14,3 @@
 })();
 
 
-// Detectar si el usuario está en un dispositivo móvil
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-// Seleccionar los elementos .about_icon y .about_icon2
-const aboutIcons = document.querySelectorAll('.about_icon, .about_icon2');
-
-// Función para activar o desactivar el efecto de hover en dispositivos móviles
-function toggleHoverEffect(event) {
-    // Verificar si el dispositivo es móvil y el tipo de evento
-    if (isMobile && event.type === 'touchstart') {
-        // Agregar la clase hover-effect cuando se toca en dispositivos móviles
-        this.classList.add('hover-effect');
-    } else {
-        // Quitar la clase hover-effect en otros casos
-        this.classList.remove('hover-effect');
-    }
-}
-
-// Agregar un listener para el evento touchstart (tapping) en dispositivos móviles
-aboutIcons.forEach(icon => {
-    icon.addEventListener('touchstart', toggleHoverEffect);
-});
-
-// Agregar un listener para el evento click en otros dispositivos
-aboutIcons.forEach(icon => {
-    icon.addEventListener('click', toggleHoverEffect);
-});
