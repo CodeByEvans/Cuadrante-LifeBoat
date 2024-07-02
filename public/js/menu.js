901f33af8ff5e92c35d89cc63b7f9b8aa5804616
EvansJLR <evansjavierlr@gmail.com>
2024-07-02 02:57:08 +0200

    Cuadrante 1.3.1

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,14 +2,16 @@
     const openButton = document.querySelector('.nav_menu');
     const menu = document.querySelector('.nav_link');
     const closeMenu = document.querySelector('.nav_close');
+    const body = document.body;
 
     openButton.addEventListener('click', () => {
         menu.classList.add('nav_link--show');
-
+        body.classList.add('no-scroll'); // Bloquea el desplazamiento del cuerpo
     });
 
     closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav_link--show');
+               body.classList.remove('no-scroll'); // Desbloquea el desplazamiento del cuerpo
     });
 })();
 
